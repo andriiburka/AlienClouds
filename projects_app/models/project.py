7160a066eecb5ac0b419b084ac3a112566c0d8cf
Andrii Burka <andriiburka@gmail.com>
2020-12-09 14:16:36 +0200

    ...

--- projects_app/models/project.py
+++ projects_app/models/project.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     project_title = models.CharField(max_length=50)
-    project_main_image = models.ImageField(upload_to='media')
+    project_main_image = models.ImageField(upload_to='media/images')
     project_description = models.TextField(blank=False)
 
     def __str__(self):
