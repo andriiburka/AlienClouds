313297c687ea0caba95b9a85ab36a94b90d0fb3d
Andrii Burka <andriiburka@gmail.com>
2020-12-19 11:40:42 +0200

    everything ok but photo edit

--- alienclouds_app/models.py
+++ alienclouds_app/models.py
@@ -3,15 +3,14 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=50, blank=False)
-    # image = models.ImageField(upload_to='images', default='logo.png')
+    title = models.CharField(max_length=30, blank=False)
     image = models.ImageField(upload_to='images', default='logo.png')
     description = models.TextField()
 
 
 # only accessible from admin page
 class ShopItem(models.Model):
-    name = models.CharField(max_length=50, blank=False)
+    name = models.CharField(max_length=30, blank=False)
     image = models.ImageField(upload_to='images', default='logo.png')
     description = models.TextField(blank=True)
     price = models.FloatField(blank=True)
